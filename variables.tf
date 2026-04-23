a719040a09c03bcde76807e8adde93bf427dd8bb
sundar <sudar@gmail.com>
2026-04-23 08:35:04 +0530

    vpc module

---
+++ variables.tf
@@ -0,0 +1,98 @@
+#### Project ####
+variable "project_name" {
+    type = string
+}
+variable "environment" {
+    type = string
+}
+#### vpc ####
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true 
+}
+variable "common_tags" {
+    type = map
+}
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
+
+#### igw ####
+variable "igw_tags" {
+    type = map
+    default = {}
+}
+#### public subnet ####
+variable "public_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "please give 2 valid public subent cidr"
+    }
+}
+variable "public_subnet_tags" {
+    type = map
+    default = {}
+}
+#### private subnet ####
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "please give 2 valid private subent cidr"
+    }
+}
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+#### database subnet ####
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "please give 2 valid database subent cidr"
+    }
+}
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+#### NAT gateway ####
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+}
+#### Routable ####
+variable "public_route_table_tags" {
+    type = map 
+    default = {}
+}
+variable "private_route_table_tags" {
+    type = map 
+    default = {}
+}
+variable "database_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+#### peering
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+variable "acceptor_vpc" {
+    type = string
+    default = ""
+}
+variable "peering_tags" {
+    type = map
+    default = {}
+}
---
+++ vpc.tf
@@ -0,0 +1,165 @@
+### vpc 
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+### internet  gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+#### Public Subnet 
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  availability_zone = local.zones[count.index]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_tags,
+    {
+        Name = "${local.resource_name}-public-${local.zones[count.index]}"
+    }
+  )
+}
+
+#### Private subnet 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  availability_zone = local.zones[count.index]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.private_subnet_tags,
+    {
+        Name = "${local.resource_name}-private-${local.zones[count.index]}"
+    }
+  )
+}
+#### database subnet
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  availability_zone = local.zones[count.index]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_tags,
+    {
+        Name = "${local.resource_name}-database-${local.zones[count.index]}"
+    }
+  )
+}
+#### elastic ip
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+#### nat gate way 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+        Name = "${local.resource_name}"
+    }
+  )
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+#### Public Route table 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags,
+    {
+        Name = "${local.resource_name}-public"
+    }
+  )
+}
+#### Private Route table 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags,
+    {
+        Name = "${local.resource_name}-private"
+    }
+  )
+}
+#### Database Route table
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags,
+    {
+        Name = "${local.resource_name}-database"
+    }
+  )
+}
+
+#### public route 
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+#### private route 
+resource "aws_route" "private_nat" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat.id
+}
+#### database route 
+resource "aws_route" "database_nat" {
+  route_table_id = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat.id
+}
+#### subnet and route table association 
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = element(aws_subnet.database[*].id, count.index)
+  route_table_id = aws_route_table.database.id
+}
